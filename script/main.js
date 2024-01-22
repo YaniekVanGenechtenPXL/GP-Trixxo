d29e758e59ea1f402a3dfe3ff059d43bf5824406
YaniekVanGenechtenPXL <yaniekvangenechten@student.pxl.be>
2024-01-22 14:57:20 +0100

    Commit 6 - Start Schade - Meeting Nicolas 22/01

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+document.getElementById('sleutels__count__container').style.visibility = "hidden";
+
 const currentDate = new Date()
 
 const year = currentDate.getFullYear();
@@ -5,3 +7,32 @@ const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Maand
 const day = currentDate.getDate().toString().padStart(2, '0');
 
 document.getElementById('datum').value = `${year}-${month}-${day}`;
+
+let sleutels = document.getElementById('sleutels');
+
+sleutels.addEventListener('change', function() {
+    if(sleutels.checked) {
+        document.getElementById('sleutels__count__container').style.visibility = "visible";
+    } else {
+        document.getElementById('sleutels__count__container').style.visibility = "hidden";
+    }
+})
+
+let sleutelsAantal = 2
+updateSleutelsAantal()
+
+document.getElementById('sleutels__minus').addEventListener('click', function() {
+    if(sleutelsAantal != 1) {
+        sleutelsAantal --
+    }
+    updateSleutelsAantal()
+});
+
+document.getElementById('sleutels__plus').addEventListener('click', function() {
+    sleutelsAantal ++
+    updateSleutelsAantal()
+});
+
+function updateSleutelsAantal() {
+    document.getElementById('sleutels__count').innerText = sleutelsAantal;
+}
